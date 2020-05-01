1ba5800572c06ea3319f5b832f8e2aac14f3e829
GeekyRahulVk <64090368+GeekyRahulVk@users.noreply.github.com>
2020-05-01 18:29:02 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pymysql
+
+app = Flask(__name__)
+
+
+host = "remotemysql.com"
+user = "UdI8EZnZLb"
+password = "iavASad5px"
+db = "UdI8EZnZLb"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def employees():
+    if request.method == "POST":
+        details = request.form
+        at_no = details['atomic_number']
+        con = pymysql.connect(host=host, user=user, password=password,
+                              db=db, cursorclass=pymysql.cursors.DictCursor)
+        cur = con.cursor()
+        cur.execute(
+            "SELECT name,atomic_number,atomic_weight,symbol,electron_configuration FROM elements where atomic_number in (%s)",(at_no,))
+        result = cur.fetchall()
+        res = result
+        return render_template('employees.html', result=res, content_type='application/json')
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
